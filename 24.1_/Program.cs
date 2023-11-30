8455e9aad0a0fea4f9eacffaf383c997b2f2cb7d
R1cky <R1cky@PC-RICKY>
2023-11-30 21:54:35 +0100

    Aggiungere i file di progetto.

---
+++ 24.1_/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrato quadrato = new Quadrato();
+            quadrato.L = float.Parse(Console.ReadLine());
+            Console.Write(quadrato.L * quadrato.L);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 24.1_/Quadrato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1_
+{
+    internal class Quadrato
+    {
+        public double L { get; set; }
+    }
+}
